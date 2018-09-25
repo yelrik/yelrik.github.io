5f16eefb35a03b3c3ac0458d0e63f709b6e57e3c
Your Name <you@example.com>
2018-09-25 15:17:20 +0000

    31

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
 
-var deferredPrompt;
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
@@ -8,10 +6,3 @@ if ('serviceWorker' in navigator) {
       console.log('Service worker registered!');
     });
 }
-
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
